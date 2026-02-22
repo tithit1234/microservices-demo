c9414adc12b66a26b6046915240462958006b14f
Haradhan Thakur <haradhant@example.com>
2026-02-22 12:34:37 +0530

    UpdatedFilesForFeignClient

--- order-service/order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.order_service.controller;
 
+import com.example.order_service.client.UserClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,29 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/orders")
 public class OrderController {
 
-    @GetMapping("/{id}")
+   /* @GetMapping("/{id}")
     public ResponseEntity<String> getOrder(@PathVariable String id) {
         // Call User Service to fetch user info
         RestTemplate restTemplate = new RestTemplate();
         String user = restTemplate.getForObject("http://localhost:8081/users/" + id, String.class);
 
         return ResponseEntity.ok("Order for " + user);
+    }*/
+
+
+    private final UserClient userClient;
+
+    public OrderController(UserClient userClient) {
+        this.userClient = userClient;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getOrder(@PathVariable String id) {
+        String user = userClient.getUser(id);
+        System.out.println("user-------> " + user);
+        return ResponseEntity.ok("Order for " + user);
+    }
+
+
 }
 
